30ad83f4f319503df2da380ead911b4773b461f2
Blancy <71817258+HiBlancy@users.noreply.github.com>
2022-12-18 23:14:35 +0100

    Update Game.cs

--- Assets/Scripts/GameSciptsControl/Game.cs
+++ Assets/Scripts/GameSciptsControl/Game.cs
@@ -4,24 +4,21 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    public static Game obj;
+    public static Game obj { get; private set; }
 
     public int maxLives = 3;
 
     public int score;
-
     void Awake()
     {
-        obj = this;
+        if (obj != null && obj != this)
+            Destroy(this);
+        else
+            obj = this;
     }
 
     public void addScore(int giveScore)
     {
         score += giveScore;
-    }
-
-    void OnDestroy()
-    {
-        obj = null;
     }
 }
